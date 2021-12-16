49197df5ff6b453736b18bb440ac229d1f15d696
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2021-12-16 16:29:24 -0700

    Made the checkout cart persistent across visits and added indexed DB stuff to keep things in the cart

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import './style.css';
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
+
+
+
+const Cart = () => {
+
+    const [state, dispatch] = useStoreContext();
+
+    useEffect(() => {
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+        };
+
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
+
+    function toggleCart() {
+        dispatch({ type: TOGGLE_CART });
+    }
+
+    if (!state.cartOpen) {
+        return (
+            <div className="cart-closed" onClick={toggleCart}>
+                <span
+                    role="img"
+                    aria-label="trash">🛒</span>
+            </div>
+        );
+    }
+
+    return (
+        <div className="cart">
+            <div className="close" onClick={toggleCart}>[close]</div>
+
+            <h2>Shopping Cart</h2>
+            <div>
+                <CartItem item={{ name: 'Camera', image: 'camera.jpg', price: 5, purchaseQuantity: 3 }} />
+                <CartItem item={{ name: 'Soap', image: 'soap.jpg', price: 6, purchaseQuantity: 4 }} />
+
+                <div className="flex-row space-between">
+                    <strong>Total: $0</strong>
+                    {
+                        Auth.loggedIn() ?
+                            <button>
+                                Checkout
+                </button>
+                            :
+                            <span>(log in to check out)</span>
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
